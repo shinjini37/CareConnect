cdd2dcc08b11879b448f7ca0a79b0633bcd736ce
Shinjini Saha <shinjini@mit.edu>
2016-04-13 21:11:04 -0400

    search function, only searches profiles via first name

---
+++ javascript/search.js
@@ -0,0 +1,28 @@
+$(function(){
+    // Search function to find students given first names
+    var search = function(){
+        var searchText = $("#search-text").val().toLowerCase();
+        console.log(searchText);
+        if (searchText in PROFILE_INDEX){
+            window.location.href = 'profile.html' + '?profile=' + searchText;
+        } else {
+            window.location.href = 'index.html';
+            alert("The student you are looking for does not exist");
+        }
+
+    }
+
+    $("#search-text").keyup(function(e){
+        if(e.keyCode == 13)
+        {
+            search();
+        }
+    });
+
+    $("#search-submit").click(function () {
+        search();
+    })
+
+    }
+
+)
