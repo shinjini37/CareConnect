e9bcf9ff894a60724bf0271174f16f56338d687f
Sam Lee <chsamlee@gmail.com>
2016-04-13 22:08:41 -0400

    Fix bug where time selector cannot be opened after changing week

--- javascript/main-time-dropdown.js
+++ javascript/main-time-dropdown.js
@@ -1,13 +1,26 @@
-$(function () {
-    $('.time-selector-btn').click(function () {
-        var id = $(this)[0].id;
-        var week = id.substring(id.length-3);
-        var timeSelectorElt = $('#time-selector-' + week);
-        if (timeSelectorElt.hasClass('show')) {
-            timeSelectorElt.removeClass('show');
+var createTimeSelectorElt = function (day) {
+    // generate elements
+    var baseElt = $('<div/>', {
+        class: 'time-selector-base',
+    });
+    var btnElt = $('<span/>', {
+        class: 'ui-icon ui-icon-triangle-1-s time-selector-btn',
+        id: 'time-selector-btn-' + day.toLowerCase(),
+    });
+    baseElt.append(btnElt);
+    var selectorElt = $('<div/>', {
+        class: 'time-selector',
+        id: 'time-selector-' + day.toLowerCase(),
+    });
+    baseElt.append(selectorElt);
+    // click handler for button
+    btnElt.click(function () {
+        if (selectorElt.hasClass('show')) {
+            selectorElt.removeClass('show');
         } else {
             $('.time-selector').removeClass('show');
-            timeSelectorElt.addClass('show');
+            selectorElt.addClass('show');
         }
     });
-});
+    return baseElt;
+};
