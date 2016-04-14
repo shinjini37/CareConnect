ff920c3a8bd494c32958bb6d5d0f2f2eb1425480
Sam Lee <chsamlee@gmail.com>
2016-04-13 20:43:01 -0400

    dropdown for selecting time range

---
+++ javascript/main-time-dropdown.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.time-selector-btn').click(function () {
+        var id = $(this)[0].id;
+        var week = id.substring(id.length-3);
+        var timeSelectorElt = $('#time-selector-' + week);
+        if (timeSelectorElt.hasClass('show')) {
+            timeSelectorElt.removeClass('show');
+        } else {
+            timeSelectorElt.addClass('show');
+        }
+    });
+});
