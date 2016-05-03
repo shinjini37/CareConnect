35a6bc7b10a4b68eee9dc74667a21625dcb2ebb1
Shinjini Saha <shinjini@mit.edu>
2016-05-03 19:18:13 -0400

    login from index page

--- javascript/navbar-search.js
+++ javascript/navbar-search.js
@@ -57,14 +57,58 @@ $(function(){
     var parentId = getParameterByName('parentId');
     if (parentId === null){
         $("#logo a").attr("href", "index.html");
+        var notLoggedIn =
+            '<input type="text" placeholder="email..." class="form-control" id="nav_email">'
+            +'<input type="password" placeholder="password..." class="form-control" id="nav_password">'
+            +'<button class="btn btn-primary" id="signin-btn"><span></span>Sign In</button>'
+            +'<button class="btn btn-primary" id="signup-btn"><span></span>Sign Up</button>'
+        $('#profile-nav').html(notLoggedIn);
     } else {
         $("#logo a").attr("href", "index.html" + "?parentId=" + parentId);
+
+        var loggedIn =
+            '<button class="btn btn-primary" id="profile-button-link"><span class="glyphicon glyphicon-user" style="color:#F97E76"></span> Profile</button>'
+            +'<button class="btn btn-primary" id="messages-button-link"><span class="glyphicon glyphicon-envelope" style="color:#F97E76"></span> Messages</button>'
+            +'<div class="dropdown nav-dropdown">'
+                +'<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" id="navigation-dropdown-button"><span class="glyphicon glyphicon-cog" style="color:#F97E76"></span>'
+                +'<span class="caret"></span></button>'
+                +'<ul class="dropdown-menu">'
+                    +'<li><a href="#">Settings</a></li>'
+                    +'<li><a href="#">Log Out</a></li>'
+                +'</ul>'
+            +'</div>';
+        $('#profile-nav').html(loggedIn);
     }
 
     // allow entire logo to be clicked
     $("#logo").click(function(){
         window.location.href = $("#logo a").attr("href");
     })
+
+
+    $("#signin-btn").click(function(event){
+        console.log("mo");
+        var parentEmail = $("#nav_email").val();
+        var parentPassword = $("#nav_password").val();
+        var login_parent_id;
+
+        for (var parentIndex = 0; parentIndex < PARENT_PROFILES.length; parentIndex++){
+            if (PARENT_PROFILES[parentIndex].email == parentEmail && PARENT_PROFILES[parentIndex].password == parentPassword){
+                login_parent_id = parentIndex;
+                window.location.href = 'index.html?parentId=' + parentIndex;
+                break;
+            } else {
+                //window.location.href = 'index.html';
+                $("#nav_email").val("");
+                $("#nav_password").val("");
+            }
+        }
+
+    });
+
+    $("#nav_logout").click(function(){
+        window.location.href = 'index.html';
+    });
 
 });
 
