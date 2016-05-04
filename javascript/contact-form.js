10b1e82d127339e83d3c5ca759dd1a6d021cae93
Huy <huypham@mit.edu>
2016-05-03 21:55:51 -0400

    message sent alert

--- javascript/contact-form.js
+++ javascript/contact-form.js
@@ -1,3 +1,14 @@
 $(function () {
-	
+	$('#btn-send').click(function () {
+	    var alert = $('<div/>', {
+	        class: 'alert alert-success fade in',
+	        id: 'alert'
+	    });
+	    alert.append("<a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a>");
+	    alert.append("Message sent successfully!");
+	    $('.container').prepend(alert);
+	    $('#alert').fadeTo(2000, 500).slideUp(500, function(){
+	        $("#alert").alert('close');
+	    });
+	})
 });
